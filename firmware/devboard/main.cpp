d630b236385df0e120016d4d243fe03a6f408a63
Alexandre Gauthier <alexandre.gauthier12@gmail.com>
2021-01-09 14:28:23 -0500

    created firmware solution

---
+++ firmware/devboard/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * devboard.cpp
+ *
+ * Created: 2021-01-09 2:24:01 PM
+ * Author : Alexandre Gauthier
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
